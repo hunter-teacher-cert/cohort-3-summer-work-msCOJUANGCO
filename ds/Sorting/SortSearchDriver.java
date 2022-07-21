1b4990778851eec4647d22f3f152778a62dd5860
Nicole Cojuangco <54820325+msCOJUANGCO@users.noreply.github.com>
2022-07-21 15:16:06 +0000

    commit this version before we tinker w timing

--- ds/Sorting/SortSearchDriver.java
+++ ds/Sorting/SortSearchDriver.java
@@ -5,12 +5,14 @@ public class SortSearchDriver {
     public static void main(String[] args) {
 
 
-//PART 1 *********************************************
-    SortSearch ss = new SortSearch(20);
+	// Uncomment these to test part 1
+	
+	SortSearch ss = new SortSearch(20);
 	System.out.println(ss);
 	
 		
-//PART 2 *********************************************
+	// Uncomment these to test part 2
+
 	int i;
 	i = ss.findSmallestIndex(0);
 	System.out.println("ss["+ i +"] = "+ss.get(i)+" : " + ss);
@@ -20,8 +22,9 @@ public class SortSearchDriver {
 	i = ss.findSmallestIndex(3);
 	System.out.println("ss["+ i + "] = "+ss.get(i)+" : " + ss);
 	
-//PART 3 *********************************************
+	// Uncomment these to test part 3
   System.out.println();
+      
       
 	System.out.println(ss);
 	ss.sort();
@@ -37,6 +40,32 @@ public class SortSearchDriver {
       System.out.println("Recursive Bin Search found value 5 at index " + ss.binarySearchRecursive(5, 0, ss.size()-1));
 
             System.out.println("Recursive Bin Search found value 35 at index " + ss.binarySearchRecursive(35, 0, ss.size()-1));
-	
+
+
+    int size = 1000;
+
+    long start,elapsed;
+	  start = System.currentTimeMillis();
+	    // put what you want to test here!
+    SortSearch test = new SortSearch(size);
+     //System.out.println(test); 
+    test.sort();
+     // System.out.println(test); 
+	  elapsed = System.currentTimeMillis() - start;
+	  System.out.println("Size: " + size + " Time: " + elapsed);
+
+     size = 1000;
+
+    
+	  start = System.currentTimeMillis();
+	    // put what you want to test here!
+    SortSearch test2 = new SortSearch(size);
+     //System.out.println(test); 
+    test2.sort();
+     // System.out.println(test); 
+	  elapsed = System.currentTimeMillis() - start;
+	  System.out.println("Size: " + size + " Time: " + elapsed);
+
+      
     }
 }
